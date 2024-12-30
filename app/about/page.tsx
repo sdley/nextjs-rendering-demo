140341c749eba9840862ce9ccfed241aa1b90cb1
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-30 22:35:18 +0000

    Dynamic rendering

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+    const cookieStore = cookies();
+    const theme = (await cookieStore).get("theme");
+    console.log(theme);
     console.log("About server component");
     return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
