a40fc153f99f47f542939a0647f548d863937302
Souleymane DIALLO <sdley2020@gmail.com>
2024-12-30 16:35:42 +0000

    Server and Client Components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+// directive specification
+"use client";
+
+import { useState } from "react";
+
+export default function DashboardPage() {
+    console.log("Dashboard client component");
+    const [name, setName] = useState("");
+    return (
+        <div>
+            <h1>Dashboard Page!</h1>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <p>Hello, {name}!</p>
+        </div>
+    );
+}
